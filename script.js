74f82faf60528d193a02b234c97b88bfa6342296
danicastojanovic <dnc.stjnvc@gmail.com>
2021-09-22 14:01:35 -0700

    added touch listener for mobile devices

--- script.js
+++ script.js
@@ -35,3 +35,51 @@ document.getElementById('reset').onclick = function() {
   board = new Board();
   board.initialize(renderState);
 };
+
+function swipedetect(){
+
+  var swipedir,
+  startX,
+  startY,
+  distX,
+  distY,
+  threshold = 50, //required min distance traveled to be considered swipe
+  restraint = 50, // maximum distance allowed at the same time in perpendicular direction
+  allowedTime = 500, // maximum time allowed to travel that distance
+  elapsedTime,
+  startTime;
+
+  window.addEventListener('touchstart', function(e){
+    e.preventDefault()
+      var touchobj = e.changedTouches[0]
+      swipedir = 'none'
+      // dist = 0
+      startX = touchobj.pageX
+      startY = touchobj.pageY
+      startTime = new Date().getTime() // record time when finger first makes contact with surface
+  }, {passive: false})
+
+  window.addEventListener('touchmove', function(e){
+      e.preventDefault() // prevent scrolling when inside DIV
+  }, {passive: false})
+
+  window.addEventListener('touchend', function(e){
+    e.preventDefault()
+      var touchobj = e.changedTouches[0]
+      distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+      distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+      elapsedTime = new Date().getTime() - startTime // get time elapsed
+      if (elapsedTime <= allowedTime){ // first condition for swipe met
+          if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+              swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+          }
+          else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+              swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+          }
+      }
+      console.log(swipedir);
+      board.move(swipedir, renderState);
+  }, {passive: false})
+}
+
+swipedetect();
